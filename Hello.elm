07f3ca96b6c4757896ca747d847fdd623f4d82e1
Kang-Kyu Lee <kangkyu1111@gmail.com>
2016-05-08 22:24:47 -0700

    Add a font, put it in the middle of browser window

--- Hello.elm
+++ Hello.elm
@@ -1,7 +1,22 @@
 module Hello where
 
-import Graphics.Element exposing (Element, show)
+import Graphics.Element exposing (Element, container, middle, centered)
+import Window
+import Text exposing (Text, typeface, fromString)
 
-main : Element
+helloText : Text
+helloText =
+  "Hello Elm Reactor!"
+    |> fromString
+    |> typeface ["Verdana"]
+    |> Text.height 36
+
+showMiddle : Int -> Int -> Element
+showMiddle w h =
+  helloText
+    |> centered
+    |> container w h middle
+
+main : Signal Element
 main =
-  show "Hello Elm Reactor!"
+  Signal.map2 showMiddle Window.width Window.height
